8aac859a85cc1126173aeef5707ff358e53a6b3d
maxitech <derstadlmax@gmail.com>
2025-02-15 15:29:15 +0100

    update: Changed limit switch pins.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 
 const byte ledPin = 13;
-const byte limitJ1 = 2;
-const byte limitJ2 = 3;
-const byte limitJ3 = 4;
-const byte limitJ4 = 5;
-const byte limitJ5 = 6;
-const byte limitJ6 = 7;
+const byte limitJ1 = 0;
+const byte limitJ2 = 1;
+const byte limitJ3 = 2;
+const byte limitJ4 = 3;
+const byte limitJ5 = 4;
+const byte limitJ6 = 5;
 
 const byte limitSwitches[6] = {limitJ1, limitJ2, limitJ3, limitJ4, limitJ5, limitJ6};
 
@@ -44,7 +44,7 @@ void checkLimitSwitches() {
     for (byte i = 0; i < 6; i++) {
     if(digitalRead(limitSwitches[i]) == LOW) {
       digitalWrite(ledPin, HIGH);
-      Serial.println(limitSwitches[i]);
+      Serial.println(limitSwitches[i] + 1);
     } else {
       digitalWrite(ledPin, LOW);
     }
