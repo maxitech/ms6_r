5a76e55a4374f7b85f864528aa06760527ca0b58
maxitech <derstadlmax@gmail.com>
2025-02-27 16:41:48 +0100

    feat: Add comBox with available COM devices to connect with.

--- frontend/Form1.Designer.cs
+++ frontend/Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = SystemColors.Window;
+            comboBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(35, 50);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(220, 25);
+            comboBox1.TabIndex = 0;
+            comboBox1.Text = "Select your device";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(261, 50);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 25);
+            button1.TabIndex = 1;
+            button1.Text = "Connect";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Name = "Form1";
+            Text = "MS6_R";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private Button button1;
     }
 }
--- frontend/Form1.cs
+++ frontend/Form1.cs
@@ -1,10 +1,45 @@
+using System.IO.Ports;
+using System.Management;
+
 namespace frontend
 {
     public partial class Form1 : Form
     {
+        List<string> availablePorts = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
+
+            availablePorts = LoadAvailablePorts();
+            foreach (string port in availablePorts)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+        private List<string> LoadAvailablePorts()
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM " +
+                "Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                string[] portnames = SerialPort.GetPortNames();
+
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+                List<string> portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
+
+                return portList;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
