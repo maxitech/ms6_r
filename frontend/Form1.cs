90c98df215e61447dca072b0012ed1a5583629dd
maxitech <derstadlmax@gmail.com>
2025-02-27 16:54:42 +0100

    update: - Add comments and shortend the code - Fixed naming convention error - Add `RenderPorts` method

--- frontend/Form1.cs
+++ frontend/Form1.cs
@@ -5,30 +5,37 @@ namespace frontend
 {
     public partial class Form1 : Form
     {
-        List<string> availablePorts = new List<string>();
-
         public Form1()
         {
             InitializeComponent();
 
-            availablePorts = LoadAvailablePorts();
-            foreach (string port in availablePorts)
-            {
-                comboBox1.Items.Add(port);
-            }
+            // Load and render available ports
+            RenderPorts(LoadAvailablePorts());
         }
-        private List<string> LoadAvailablePorts()
+        private static List<string> LoadAvailablePorts()
         {
+            // If interested in how this works, check out the following link: https://youtu.be/3SQayMiapKQ?si=ZZsX7aso8TKUQ_hf
             using (var searcher = new ManagementObjectSearcher("SELECT * FROM " +
                 "Win32_PnPEntity WHERE Caption like '%(COM%'"))
             {
+                // Get port names
                 string[] portnames = SerialPort.GetPortNames();
 
+                // Get caption of each port
                 var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
 
+                // Combine port names with captions
                 List<string> portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
 
                 return portList;
+            }
+        }
+
+        private void RenderPorts(List<string> availablePorts)
+        {
+            foreach (string port in availablePorts)
+            {
+                comboBox1.Items.Add(port);
             }
         }
 
