ad539cd58ba384e16ff68eda5b0ab6806f994120
maxitech <derstadlmax@gmail.com>
2025-03-06 17:20:04 +0100

    feat: Implemented basic serial comunication between teensy and gui. - Frontend can send a command and the code handles it. For now it is able to switch between two programs.

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+
+// pin definitions here:
 const byte ledPin = 13;
 const byte limitJ1 = 0;
 const byte limitJ2 = 1;
@@ -10,27 +12,84 @@ const byte limitJ6 = 5;
 
 const byte limitSwitches[6] = {limitJ1, limitJ2, limitJ3, limitJ4, limitJ5, limitJ6};
 
-//function declarations here:
 
+// function declarations here:
 void initLimitSwitches();
 void checkLimitSwitches();
+void readSerialInput();
+void handleNewCommand();
+void processPingCommand();
+void processTestSwitchesCommand();
+
+
+// global variables here:
+String command;
 
 
 void setup() {
   // setup code here:
-
-  initLimitSwitches();
-
   Serial.begin(9600);
+  initLimitSwitches();
 }
+
 
 void loop() {
   // main code here:
-
-  checkLimitSwitches();
+  readSerialInput();
+  handleNewCommand();
 }
 
+
 // function definitions here:
+void readSerialInput() {
+  if (Serial.available() > 0) {
+    String input = Serial.readString();
+    input.trim();
+    Serial.println("Loaded Program - " + input);
+    command = input;
+  }
+}
+
+
+void handleNewCommand() {
+  if (command == "PING") {
+    processPingCommand();
+  } else if (command == "TEST_SWITCHES") {
+    processTestSwitchesCommand();
+  }
+}
+
+
+void processPingCommand() {
+  delay(500);
+  bool newCommandReceived = false;
+
+  while (!newCommandReceived) {
+    Serial.println("PONG");
+    delay(1000);
+
+    if (Serial.available() > 0) {
+      readSerialInput();
+      newCommandReceived = true;
+    }
+  }
+}
+
+
+void processTestSwitchesCommand() {
+  delay(500);
+  bool newCommandReceived = false;
+
+  while (!newCommandReceived) {
+    checkLimitSwitches();
+
+    if (Serial.available() > 0) {
+      readSerialInput();
+      newCommandReceived = true;
+    }
+  }
+}
+
 
 void initLimitSwitches() {
   pinMode(ledPin, OUTPUT);
@@ -44,7 +103,7 @@ void checkLimitSwitches() {
     for (byte i = 0; i < 6; i++) {
     if(digitalRead(limitSwitches[i]) == LOW) {
       digitalWrite(ledPin, HIGH);
-      Serial.println(limitSwitches[i] + 1);
+      // Serial.println(limitSwitches[i] + 1);
     } else {
       digitalWrite(ledPin, LOW);
     }
