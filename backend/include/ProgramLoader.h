3f38e7de4d81783ae59774fc5137b2ad943b842d
maxitech <derstadlmax@gmail.com>
2025-03-19 22:16:18 +0100

    feat: Implementet `ProgramLoader` class for handling state of which program is running.

---
+++ backend/include/ProgramLoader.h
@@ -0,0 +1,33 @@
+#ifndef PROGRAMLOADER_H
+#define PROGRAMLOADER_H
+
+#include <Arduino.h>
+
+
+enum ProgramState {
+    IDLE,
+    PING,
+    PONG
+};
+
+
+class ProgramLoader {
+public: 
+    ProgramLoader();
+    void handleCommand(const std::vector<String>& cmdParts);
+    void run();
+    ProgramState getState() const;
+
+
+private:
+    void _loadProgram(const String& program);
+
+    ProgramState _setState(ProgramState newState);
+    ProgramState _currentProgramState = IDLE;
+
+    void _executePing();
+    void _executePong();
+};
+
+
+#endif //PROGRAMLOADER_H
