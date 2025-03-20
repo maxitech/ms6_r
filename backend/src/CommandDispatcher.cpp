2e2a6f5f71f7fbbe0a699c6021b35703aa3da4ab
maxitech <derstadlmax@gmail.com>
2025-03-20 11:29:56 +0100

    feat: First implementation. Class responsible for dispatching the incomming cmd to the classes it belongs to.

---
+++ backend/src/CommandDispatcher.cpp
@@ -0,0 +1,18 @@
+#include "CommandDispatcher.h"
+
+
+CommandDispatcher::CommandDispatcher(ProgramLoader& programLoader) : _programLoader(programLoader) {};
+
+
+void CommandDispatcher::dispatch(const std::vector<String>& cmdParts) {
+    if(cmdParts.empty()) return;
+
+    const String& command = cmdParts[0];
+    
+    if(command == "LOAD") {
+        _programLoader.handleCommand(cmdParts);
+    } else {
+        Serial.println("Unknown command: " + command);
+    }
+}
+
