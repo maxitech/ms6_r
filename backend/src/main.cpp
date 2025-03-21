07f271afb8400f2a08f494562f8df1044a915bdb
maxitech <derstadlmax@gmail.com>
2025-03-21 10:20:58 +0100

    update: Add pin definitions for LED pin and limit switches. Initialized classes with needed arguments. - Changed order in which classes getting initialized -> avoid potential errors - Init limit switches in the setup

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -2,22 +2,36 @@
 #include "SerialHandler.h"
 #include "CommandProcessor.h"
 #include "ProgramLoader.h"
+#include "LimitSwitches.h"
 
 
-ProgramLoader programLoader;
-SerialHandler serialHandler;
-CommandProcessor cmdProcessor(programLoader);
+// Init variables
+const byte ledPin = 13;
+
+// Limit switches
+const byte limitJ1 = 0;
+const byte limitJ2 = 1;
+const byte limitJ3 = 2;
+const byte limitJ4 = 3;
+const byte limitJ5 = 4;
+const byte limitJ6 = 5;
+const std::array<byte, 6> limitSwitchPins = {limitJ1, limitJ2, limitJ3, limitJ4, limitJ5, limitJ6};
+
+// Initialize Classes
+LimitSwitches limitSwitches(ledPin, limitSwitchPins); // Manages the state of limit switches
+ProgramLoader programLoader(limitSwitches);           // Controls loading programs and state management
+CommandProcessor cmdProcessor(programLoader);         // Parses and processes incoming commands
+SerialHandler serialHandler;                          // Handles serial communication and command routing
 
 
 void setup() {
-  // setup code here:
   Serial.begin(9600);
+  limitSwitches.init();
   serialHandler.setCommandProcessor(&cmdProcessor);
 }
 
 
 void loop() {
-  // main code here:
   serialHandler.listenForSerial();
   programLoader.run();
 }
