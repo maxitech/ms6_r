7c16bb82acd3376b750e3f089fe8d60aef06e19d
maxitech <derstadlmax@gmail.com>
2025-04-23 11:22:03 +0200

    update:  Created functions to calculate angle based on given motor configurations. Need for later calculations with kinematics.

--- backend/include/Kinematics.h
+++ backend/include/Kinematics.h
@@ -3,11 +3,21 @@
 
 #include "MotorConfig.h"
 #include <Arduino.h>
+#include <cmath>
 
 class Kinematics
 {
 public:
-    explicit Kinematics(std::vector<MotorConfig>& motorConfigs) {};
+    explicit Kinematics(std::vector<MotorConfig>& motorConfigs);
+
+private:
+    float _totalRatio(const MotorConfig& motorConfig) const;
+    float _stepsPerRev(const MotorConfig& motorConfig) const;
+    float _stepsToDeg(const MotorConfig& motorConfig, const int currPosInSteps) const;
+    int   _degToSteps(const MotorConfig& motorConfig, const float angle) const;
+    float _degToRad(const float angle) const;
+
+    std::vector<MotorConfig> _motorConfigs;
 };
 
 #endif // KINEMATICS_H
