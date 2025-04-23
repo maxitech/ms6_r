2f760a0d645c859b5cbd0909446fead55aaec9de
maxitech <derstadlmax@gmail.com>
2025-04-23 14:15:43 +0200

    refactor: Add `_` prefix to private functions and implemented logic for converting radian to degree & logic to get back all joint angles(radian) in a vector.

--- backend/src/Kinematics.cpp
+++ backend/src/Kinematics.cpp
@@ -1,11 +1,8 @@
 #include "Kinematics.h"
 
-Kinematics::Kinematics(std::vector<MotorConfig>& motorConfigs)
+Kinematics::Kinematics(const std::vector<MotorConfig>& motorConfigs)
     : _motorConfigs(motorConfigs)
 {
-    for (auto& motorConfig : _motorConfigs)
-    {
-    }
 }
 
 float Kinematics::_totalRatio(const MotorConfig& motorConfig) const
@@ -18,17 +15,36 @@ float Kinematics::_stepsPerRev(const MotorConfig& motorConfig) const
     return (motorConfig.stepsPerRev * motorConfig.microsteps * _totalRatio(motorConfig));
 }
 
-float Kinematics::stepsToDeg(const MotorConfig& motorConfig, const int currPosInSteps) const
+float Kinematics::_stepsToDeg(const MotorConfig& motorConfig, const int currPosInSteps) const
 {
     return (static_cast<float>(currPosInSteps) / _stepsPerRev(motorConfig)) * 360.0f;
 }
 
-int Kinematics::degToSteps(const MotorConfig& motorConfig, const float deg) const
+int Kinematics::_degToSteps(const MotorConfig& motorConfig, const float deg) const
 {
     return static_cast<int>((deg / 360.0f) * _stepsPerRev(motorConfig));
 }
 
-float Kinematics::degToRad(const float deg) const
+float Kinematics::_degToRad(const float deg) const
 {
     return deg * (M_PI / 180.0f);
+}
+
+float Kinematics::_radToDeg(const float rad) const
+{
+    return rad * (180.0f / M_PI);
+}
+
+std::vector<float> Kinematics::getJointAnglesInRad() const
+{
+    std::vector<float> angles;
+    angles.reserve(_motorConfigs.size());
+
+    for (const auto& cfg : _motorConfigs)
+    {
+        float deg = _stepsToDeg(cfg, cfg.motor->getPosition());
+        angles.push_back(_degToRad(deg));
+    }
+
+    return angles;
 }
