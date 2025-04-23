d3a539c8506e49d131f17e5f3d01178b06b9503c
maxitech <derstadlmax@gmail.com>
2025-04-23 11:44:27 +0200

    refactor: Make `stepsToDeg, degToSteps and degToRad` public functions.

--- backend/include/Kinematics.h
+++ backend/include/Kinematics.h
@@ -10,12 +10,13 @@ class Kinematics
 public:
     explicit Kinematics(std::vector<MotorConfig>& motorConfigs);
 
+    float stepsToDeg(const MotorConfig& motorConfig, const int currPosInSteps) const;
+    int   degToSteps(const MotorConfig& motorConfig, const float deg) const;
+    float degToRad(const float deg) const;
+
 private:
     float _totalRatio(const MotorConfig& motorConfig) const;
     float _stepsPerRev(const MotorConfig& motorConfig) const;
-    float _stepsToDeg(const MotorConfig& motorConfig, const int currPosInSteps) const;
-    int   _degToSteps(const MotorConfig& motorConfig, const float angle) const;
-    float _degToRad(const float angle) const;
 
     std::vector<MotorConfig> _motorConfigs;
 };
