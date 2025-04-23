0e96090fe9cd99f1c013f838cd4183505d16e584
maxitech <derstadlmax@gmail.com>
2025-04-23 14:12:53 +0200

    refactor: Make prev. public functions private and created  function which returns a vector of all angles in radian.

--- backend/include/Kinematics.h
+++ backend/include/Kinematics.h
@@ -8,17 +8,19 @@
 class Kinematics
 {
 public:
-    explicit Kinematics(std::vector<MotorConfig>& motorConfigs);
+    explicit Kinematics(const std::vector<MotorConfig>& motorConfigs);
 
-    float stepsToDeg(const MotorConfig& motorConfig, const int currPosInSteps) const;
-    int   degToSteps(const MotorConfig& motorConfig, const float deg) const;
-    float degToRad(const float deg) const;
+    std::vector<float> getJointAnglesInRad() const;
 
 private:
     float _totalRatio(const MotorConfig& motorConfig) const;
     float _stepsPerRev(const MotorConfig& motorConfig) const;
+    float _stepsToDeg(const MotorConfig& motorConfig, const int currPosInSteps) const;
+    int   _degToSteps(const MotorConfig& motorConfig, const float deg) const;
+    float _degToRad(const float deg) const;
+    float _radToDeg(const float rad) const;
 
-    std::vector<MotorConfig> _motorConfigs;
+    const std::vector<MotorConfig>& _motorConfigs;
 };
 
 #endif // KINEMATICS_H
