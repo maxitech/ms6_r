0d4c35e7a6945f8acc4db53d1115718480267982
maxitech <derstadlmax@gmail.com>
2025-04-23 11:25:59 +0200

    add: Required calculations for step-to-degree, degree-to-step, and ratio conversions for future kinematic calculations.

--- backend/src/Kinematics.cpp
+++ backend/src/Kinematics.cpp
@@ -8,11 +8,27 @@ Kinematics::Kinematics(std::vector<MotorConfig>& motorConfigs)
     }
 }
 
-float Kinematics::_calcAngle(const MotorConfig& _motorConfig, const int currPosInSteps) const
+float Kinematics::_totalRatio(const MotorConfig& motorConfig) const
 {
-    float gearRatio        = _motorConfig.drivenTeeth / _motorConfig.driverTeeth;
-    float totalRatio       = _motorConfig.gearboxRatio * gearRatio;
-    float totalStepsPerRev = _motorConfig.stepsPerRev * _motorConfig.microsteps * totalRatio;
-    float angle            = (static_cast<float>(currPosInSteps) / totalStepsPerRev) * 360.0f;
-    return angle;
+    return (motorConfig.gearboxRatio * (motorConfig.drivenTeeth / motorConfig.driverTeeth));
+}
+
+float Kinematics::_stepsPerRev(const MotorConfig& motorConfig) const
+{
+    return (motorConfig.stepsPerRev * motorConfig.microsteps * _totalRatio(motorConfig));
+}
+
+float Kinematics::_stepsToDeg(const MotorConfig& _motorConfig, const int currPosInSteps) const
+{
+    return (static_cast<float>(currPosInSteps) / _stepsPerRev(_motorConfig)) * 360.0f;
+}
+
+int Kinematics::_degToSteps(const MotorConfig& _motorConfig, const float deg) const
+{
+    return static_cast<int>((deg / 360.0f) * _stepsPerRev(_motorConfig));
+}
+
+float Kinematics::_degToRad(const float deg) const
+{
+    return deg * (M_PI / 180.0f);
 }
