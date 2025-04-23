98af11e3d0aaf2d81f3ae8fec6db2843bed82ed7
maxitech <derstadlmax@gmail.com>
2025-04-23 10:25:26 +0200

    update: Implement constructor with default values for optional init.

--- backend/include/MotorConfig.h
+++ backend/include/MotorConfig.h
@@ -8,6 +8,15 @@ struct MotorConfig
     float driverTeeth;
     float drivenTeeth;
     float gearboxRatio;
+
+    MotorConfig(int spr, int ms, float drt = 1.0f, float dnt = 1.0f, float gr = 1.0f)
+        : stepsPerRev(spr)
+        , microsteps(ms)
+        , driverTeeth(drt)
+        , drivenTeeth(dnt)
+        , gearboxRatio(gr)
+    {
+    }
 };
 
 #endif // MOTORCONFIG_H
