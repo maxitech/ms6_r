181b62da6993296da2987c6ecaa7b2f8e0d45479
maxitech <derstadlmax@gmail.com>
2025-04-23 11:45:19 +0200

    refactor: Removed `_` prefix because the methods are public now.

--- backend/src/Kinematics.cpp
+++ backend/src/Kinematics.cpp
@@ -18,17 +18,17 @@ float Kinematics::_stepsPerRev(const MotorConfig& motorConfig) const
     return (motorConfig.stepsPerRev * motorConfig.microsteps * _totalRatio(motorConfig));
 }
 
-float Kinematics::_stepsToDeg(const MotorConfig& _motorConfig, const int currPosInSteps) const
+float Kinematics::stepsToDeg(const MotorConfig& motorConfig, const int currPosInSteps) const
 {
-    return (static_cast<float>(currPosInSteps) / _stepsPerRev(_motorConfig)) * 360.0f;
+    return (static_cast<float>(currPosInSteps) / _stepsPerRev(motorConfig)) * 360.0f;
 }
 
-int Kinematics::_degToSteps(const MotorConfig& _motorConfig, const float deg) const
+int Kinematics::degToSteps(const MotorConfig& motorConfig, const float deg) const
 {
-    return static_cast<int>((deg / 360.0f) * _stepsPerRev(_motorConfig));
+    return static_cast<int>((deg / 360.0f) * _stepsPerRev(motorConfig));
 }
 
-float Kinematics::_degToRad(const float deg) const
+float Kinematics::degToRad(const float deg) const
 {
     return deg * (M_PI / 180.0f);
 }
