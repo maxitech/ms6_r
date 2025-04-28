9fba32830942ee9361bc8ee9a1e59f5030838848
maxitech <derstadlmax@gmail.com>
2025-04-28 18:38:19 +0200

    add: Class dummy for testing.

---
+++ backend/test/dummy/d_teensystep4.h
@@ -0,0 +1,13 @@
+#pragma once
+// Dummy version of TeensyStep4 for native tests
+
+namespace TS4
+{
+
+    class Stepper
+    {
+    public:
+        float getPosition() const {}
+    };
+
+} // namespace TS4
