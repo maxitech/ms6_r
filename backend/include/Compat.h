59f9b239625e16439273f38d17982ee045946d35
maxitech <derstadlmax@gmail.com>
2025-04-28 18:34:40 +0200

    add: Compatibility file to conditionally import needed includes. Enables Crossplatform support.

---
+++ backend/include/Compat.h
@@ -0,0 +1,28 @@
+#ifndef COMPAT_H
+#define COMPAT_H
+
+#if __has_include(<ArduinoEigenDense.h>)
+#include <ArduinoEigenDense.h>
+#else
+#include <Eigen/Dense>
+#endif
+
+#ifdef TEENSY41
+#include "teensystep4.h"
+#include <Arduino.h>
+#else
+#include "d_teensystep4.h"
+#include <iostream>
+#endif
+
+// Cross-Platform Logging
+inline void logMessage(const char* message)
+{
+#if defined(UNIT_TEST) && !defined(TEENSY41)
+    std::cout << "[LOG] " << message << std::endl;
+#else
+    Serial.println(message);
+#endif
+}
+
+#endif // COMPAT_H
