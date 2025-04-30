5cbf1556e46398fa99f0fe04c8331677798387be
maxitech <derstadlmax@gmail.com>
2025-04-30 14:12:24 +0200

    refactor: Changed raw pointer to smartpointer.

--- backend/include/MotorConfig.h
+++ backend/include/MotorConfig.h
@@ -2,20 +2,21 @@
 #define MOTORCONFIG_H
 
 #include "Compat.h"
+#include <memory>
 
 using namespace TS4;
 
 struct MotorConfig
 {
-    Stepper* motor;
-    int      stepsPerRev;
-    int      microsteps;
-    float    driverTeeth;
-    float    drivenTeeth;
-    float    gearboxRatio;
+    std::shared_ptr<Stepper> motor;
+    int                      stepsPerRev;
+    int                      microsteps;
+    float                    driverTeeth;
+    float                    drivenTeeth;
+    float                    gearboxRatio;
 
-    MotorConfig(Stepper& motor, int spr, int ms, float drt = 1.0f, float dnt = 1.0f, float gr = 1.0f)
-        : motor(&motor)
+    MotorConfig(std::shared_ptr<Stepper> motor, int spr, int ms, float drt = 1.0f, float dnt = 1.0f, float gr = 1.0f)
+        : motor(motor)
         , stepsPerRev(spr)
         , microsteps(ms)
         , driverTeeth(drt)
