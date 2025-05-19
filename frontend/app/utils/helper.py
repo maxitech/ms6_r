17e0ee2b42f525005c6a3e2333a30b45b085b96a
maxitech <derstadlmax@gmail.com>
2025-05-19 17:24:18 +0200

    add: Helper function to calculate checksum for serial com.

---
+++ frontend/app/utils/helper.py
@@ -0,0 +1,12 @@
+class Helper:
+    def __init__(self):
+        pass
+
+    def calc_checksum(self, data):
+        checksumVal = 0
+        for char in data:
+            checksumVal ^= ord(char)
+
+        hex_checksum = format(checksumVal, "X").zfill(2)
+        print(hex_checksum)
+        return hex_checksum
