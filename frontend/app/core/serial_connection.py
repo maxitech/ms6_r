a889f182b0866d74a47f79ac94bfc1db76ce4308
maxitech <derstadlmax@gmail.com>
2025-05-20 14:44:55 +0200

    refactor: Updated `send_data` func. with proper error handling. and uppdated `connect` & `disconnect` to work correctly with the `SerialWorker` class.

--- frontend/app/core/serial_connection.py
+++ frontend/app/core/serial_connection.py
@@ -1,8 +1,11 @@
 import serial.tools.list_ports
+import serial
+from app.core.serial_worker import SerialWorker
 
 
 class SerialConnection:
     def __init__(self, baudrate=115200):
+        self._serial_worker = None
         self._baudrate = baudrate
         self._serial = None
         self._ports = []
@@ -18,14 +21,21 @@ class SerialConnection:
     def setPort(self, port):
         self._port = port
 
-    def connect(self):
+    def connect(self, update_ui_callback):
         try:
-            self._serial = serial.Serial(self._port, self._baudrate)
+            self._serial = serial.Serial(
+                self._port, self._baudrate, timeout=1, write_timeout=1
+            )
+            self._serial_worker = SerialWorker(self._serial)
+            self._serial_worker.data_received.connect(update_ui_callback)
+            self._serial_worker.start()
             print(f"Connected to {self._port} at {self._baudrate} baud.")
         except serial.SerialException as e:
             print(f"Error connecting to {self._port}: {e}")
 
     def disconnect(self):
+        if self._serial_worker:
+            self._serial_worker.stop()
         if self._serial and self._serial.is_open:
             self._serial.close()
             print(f"Disconnected from {self._port}.")
@@ -46,10 +56,19 @@ class SerialConnection:
         return False
 
     def send_data(self, data):
+        if not self.is_connected():
+            print(f"Cannot send data, no active connection to {self._port}")
+            return
+
         if self.is_connected():
             if isinstance(data, str):
                 data = bytes(data, "utf-8")
-            self._serial.write(data)
+            try:
+                self._serial.write(data)
+            except serial.SerialTimeoutException:
+                print("Write timeout error!")
+            except Exception as e:
+                print(f"[MainWindow] Write error: {e}")
 
     # private methods
     def _load_ports(self):
