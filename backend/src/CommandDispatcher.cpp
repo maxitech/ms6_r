1828a5562355e4809cf5a9b46b24d8d4329dce35
maxitech <derstadlmax@gmail.com>
2025-05-23 12:03:36 +0200

    refactor: Disabled manual loading of main function -> backend is now able to auto run main if needed. Comment out manual version in case it is needed or wanted in the future.

--- backend/src/CommandDispatcher.cpp
+++ backend/src/CommandDispatcher.cpp
@@ -10,23 +10,35 @@ void CommandDispatcher::dispatch(const std::vector<String>& cmdParts)
 
     const String& command = cmdParts[0];
 
-    if (command == "LOAD")
+    // --------------------This block is use if backend runs main func automatically if JOG of MOVE are the cmd's-------------
+    if (command == "LOAD" || command == "JOG" || command == "MOVE")
     {
         _programLoader.handleCommand(cmdParts);
     }
-    else if (command == "JOG" || command == "MOVE")
-        if (_programLoader.getState() == MAIN)
-        {
-            _programLoader.handleCommand(cmdParts);
-        }
-        else
-        {
-            Serial.println("Error: Main program must be loaded first!");
-            delay(20);
-        }
     else
     {
         Serial.println("Unknown command: " + command);
         delay(20);
     }
+
+    // -------------------This block can be used if main func running has to be handled by the frontend------------------------
+    // if (command == "LOAD")
+    // {
+    //     _programLoader.handleCommand(cmdParts);
+    // }
+    // else if (command == "JOG" || command == "MOVE")
+    //     if (_programLoader.getState() == MAIN)
+    //     {
+    //         _programLoader.handleCommand(cmdParts);
+    //     }
+    //     else
+    //     {
+    //         Serial.println("Error: Main program must be loaded first!");
+    //         delay(20);
+    //     }
+    // else
+    // {
+    //     Serial.println("Unknown command: " + command);
+    //     delay(20);
+    // }
 }
