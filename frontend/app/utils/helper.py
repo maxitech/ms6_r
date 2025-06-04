269cbc7dcd66bb557d4bfb4ee290511b3fd43a12
maxitech <derstadlmax@gmail.com>
2025-06-04 15:38:15 +0200

    add: Two functions for the jog slider fature. - Map percentage values from the slider to the speeds which are calculated based on v_min, v_max, slider_value and gear_factors-> factors if a gear has ratio.

--- frontend/app/utils/helper.py
+++ frontend/app/utils/helper.py
@@ -1,7 +1,11 @@
+import math
+
+
 class Helper:
     def __init__(self):
         pass
 
+    # *************************Public Methods****************************
     def calc_checksum(self, data):
         checksumVal = 0
         for char in data:
@@ -9,3 +13,13 @@ class Helper:
 
         hex_checksum = format(checksumVal, "X").zfill(2)
         return hex_checksum
+
+    def get_joint_speeds(self, slider_value, v_min, v_max, gear_factors):
+        base_speed = self._map_slider_to_speed(slider_value, v_min, v_max)
+        return [round(base_speed * math.sqrt(f)) for f in gear_factors]
+
+    # *************************Private Methods****************************
+    def _map_slider_to_speed(self, slider_value, v_min, v_max):
+        # t = (slider_value - 10) / (100 - 10)  # Normalize slider value from 10 to 100
+        percent = slider_value / 100
+        return v_min + percent * (v_max - v_min)
