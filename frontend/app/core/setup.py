59f485ec4cf4a68805e33d2d9209be785521fa78
maxitech <derstadlmax@gmail.com>
2025-06-06 14:54:51 +0200

    add: Function for default init. the dicts.

--- frontend/app/core/setup.py
+++ frontend/app/core/setup.py
@@ -1,18 +1,50 @@
 class Setup:
     def __init__(self):
         self._default_val = 0
-        self._dicts = self._init_dicts_with_def_val()
 
     # *************************Public Methods****************************
     def save(self):
         pass
 
     def get_setup(self):
-        return self._dicts
+        pass
 
     # *************************Private Methods****************************
     def _init_dicts_with_def_val(self):
-        pass
+        default_dicts = {
+            "dh_params": {
+                "joint1": ["theta_offset", "alpha", "d", "a"],
+                "joint2": ["theta_offset", "alpha", "d", "a"],
+                "joint3": ["theta_offset", "alpha", "d", "a"],
+                "joint4": ["theta_offset", "alpha", "d", "a"],
+                "joint5": ["theta_offset", "alpha", "d", "a"],
+                "joint6": ["theta_offset", "alpha", "d", "a"],
+            },
+            "homing_params": {
+                "motor1": ["vel", "away_vel", "back_vel", "home_pos"],
+                "motor2": ["vel", "away_vel", "back_vel", "home_pos"],
+                "motor3": ["vel", "away_vel", "back_vel", "home_pos"],
+                "motor4": ["vel", "away_vel", "back_vel", "home_pos"],
+                "motor5": ["vel", "away_vel", "back_vel", "home_pos"],
+                "motor6": ["vel", "away_vel", "back_vel", "home_pos"],
+            },
+            "speed_a_accel": {
+                "motor1": ["max_speed", "acc"],
+                "motor2": ["max_speed", "acc"],
+                "motor3": ["max_speed", "acc"],
+                "motor4": ["max_speed", "acc"],
+                "motor5": ["max_speed", "acc"],
+                "motor6": ["max_speed", "acc"],
+            },
+        }
+
+        return {
+            key: {
+                sub_key: {field: self._default_val for field in val}
+                for sub_key, val in values.items()
+            }
+            for key, values in default_dicts.items()
+        }
 
     def _get_field_vals(self):
         pass
@@ -33,6 +65,3 @@ class Setup:
         except:
             pass
         pass
-
-
-Setup()._init_dicts_with_def_vals()
