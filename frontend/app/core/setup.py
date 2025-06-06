3295ee98c328853de3a2eee6afaf9e66749b4d0c
maxitech <derstadlmax@gmail.com>
2025-06-06 10:38:55 +0200

    add: Structure of the setup class.

---
+++ frontend/app/core/setup.py
@@ -0,0 +1,38 @@
+class Setup:
+    def __init__(self):
+        self._default_val = 0
+        self._dicts = self._init_dicts_with_def_val()
+
+    # *************************Public Methods****************************
+    def save(self):
+        pass
+
+    def get_setup(self):
+        return self._dicts
+
+    # *************************Private Methods****************************
+    def _init_dicts_with_def_val(self):
+        pass
+
+    def _get_field_vals(self):
+        pass
+
+    def _detect_changes(self):
+        pass
+
+    def _write_setup_to_file(self):
+        try:
+            pass
+        except:
+            pass
+        pass
+
+    def _read_setup_from_file(self):
+        try:
+            pass
+        except:
+            pass
+        pass
+
+
+Setup()._init_dicts_with_def_vals()
