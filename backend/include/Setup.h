ab4e1069bc88b07bcaaf15fe390858a710a7edb1
maxitech <derstadlmax@gmail.com>
2025-06-12 16:17:34 +0200

    add: Setup.h & declaration of `_parseDHParams` function.

---
+++ backend/include/Setup.h
@@ -0,0 +1,18 @@
+#ifndef SETUP_H
+#define SETUP_H
+
+#include "Kinematics.h"
+#include <ArduinoJson.h>
+
+class Setup
+{
+public:
+    explicit Setup(const String& jsonString);
+
+private:
+    std::vector<DHparam> _parseDHParams();
+
+    const String _jsonStr;
+};
+
+#endif SETUP_H // SETUP_H
