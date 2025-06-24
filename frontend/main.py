a14b955a567fa7046cdd581201682bc4e04d1f88
maxitech <derstadlmax@gmail.com>
2025-06-24 09:49:52 +0200

    add: Fix for layout and styleing problems on mac.

--- frontend/main.py
+++ frontend/main.py
@@ -2,6 +2,8 @@ import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from app.ui_gen.main_window_ui import Ui_MainWindow
 from app.controllers.main_window_controller import MainWindowController
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
@@ -18,7 +20,10 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == "__main__":
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
     app = QApplication(sys.argv)
+    app.setStyle("Fusion")
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
