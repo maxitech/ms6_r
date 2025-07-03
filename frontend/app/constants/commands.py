0ae8d4463709d8acb417b67414454dff8efa454e
maxitech <derstadlmax@gmail.com>
2025-07-03 15:42:38 +0200

    refactor: Split `main_window_controller.py` into seperate smaller classes for better read & maintainability. Related refactor changes.

---
+++ frontend/app/constants/commands.py
@@ -0,0 +1,12 @@
+COMMAND_MAP = {
+    "cmd_ping_btn": ("Ping", "LOAD,[PING]"),
+    "cmd_pong_btn": ("Pong", "LOAD,[PONG]"),
+    "cmd_switches_btn": ("TestSwitches", "LOAD,[TEST_SWITCHES]"),
+    "cmd_home_axis_btn": ("Home", "LOAD,[HOME]"),
+}
+
+# JOG_CONFIG = {
+#     "gear_factors": [1, 14, 1, 1, 1, 1],
+#     "speed_range": {"min": 500, "max": 4000},
+#     "slider_step": 10,
+# }
