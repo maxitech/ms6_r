d582d9a2e97e97b2009e54438d560e83a1ddf521
maxitech <derstadlmax@gmail.com>
2025-07-07 17:09:33 +0200

    refactor: Put `JogState` enum into its own file -> prevents the need of duplicated code.

---
+++ backend/include/JogState.h
@@ -0,0 +1,10 @@
+#ifndef JOGSTATE_H
+#define JOGSTATE_H
+
+enum JogState
+{
+    IDLE_JOG, ///< No jog command is active.
+    JOGGING,  ///< Currently executing a jog command.
+};
+
+#endif // JOSTATE_H
