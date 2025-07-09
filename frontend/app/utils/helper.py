31d68255f69af9f8fccf38dbf22859b7d4a6555f
maxitech <derstadlmax@gmail.com>
2025-07-09 17:54:34 +0200

    add: Two new mapping functions for cartesian jog feature.

--- frontend/app/utils/helper.py
+++ frontend/app/utils/helper.py
@@ -18,6 +18,14 @@ class Helper:
         base_speed = self._map_slider_to_speed(slider_value, v_min, v_max)
         return [round(base_speed * math.sqrt(f)) for f in gear_factors]
 
+    def map_slider_to_linear_speed(self, slider_val, min_mm_s=1.0, max_mm_s=100.0):
+        percent = slider_val / 100  # 100 max_slider_val
+        return min_mm_s + percent * (max_mm_s - min_mm_s)
+
+    def map_slider_to_rot_speed(self, slider_val, min_deg_s=1.0, max_deg_s=100.0):
+        percent = slider_val / 100
+        return min_deg_s + percent * (max_deg_s - min_deg_s)
+
     # *************************Private Methods****************************
     def _map_slider_to_speed(self, slider_value, v_min, v_max):
         percent = slider_value / 100
