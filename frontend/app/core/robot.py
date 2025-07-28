31d8a0007ce5669f17185ba7333b8713e38b1810
maxitech <derstadlmax@gmail.com>
2025-07-28 10:37:01 +0200

    add: Core robot class, responsible for robots functionality (WIP).

---
+++ frontend/app/core/robot.py
@@ -0,0 +1,36 @@
+import roboticstoolbox as rtb
+from roboticstoolbox import *
+from math import pi
+import numpy as np
+import matplotlib.pyplot as plt
+
+from app.constants.ms6_r_constants import ms6_r_constants
+
+
+class Robot(DHRobot):
+    def __init__(self, constants):
+        dh = constants.DH_PARAMS
+        super().__init__(
+            [
+                RevoluteDH(
+                    d=dh["d"][i],
+                    a=dh["a"][i],
+                    alpha=dh["alpha"][i],
+                    offset=dh["offset"][i],
+                )
+                for i in range(6)
+            ],
+            name=constants.NAME,
+        )
+
+        self.constants = constants
+
+
+if __name__ == "__main__":
+    # Debug
+    ms6_r = Robot(constants=ms6_r_constants)
+    q = [0, 0, 0, 0, np.deg2rad(90), 0]
+    T = ms6_r.fkine(q=q)
+    print(T)
+    sol = ms6_r.ikine_LM(T)
+    ms6_r.plot(q=q, backend="pyplot", block=True)
