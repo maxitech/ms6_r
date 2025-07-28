b8e6a64c2e23f336180e0cd7705a8577fb0ad83c
maxitech <derstadlmax@gmail.com>
2025-07-28 10:34:47 +0200

    add: Data class for robot constants.

---
+++ frontend/app/constants/robot_constants.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass(frozen=True)
+class RobotConstants:
+    STEPS_PER_REV: List[int]
+    MICRO_STEPS: int
+    MOTOR_SPEED_A_ACCEL: List[List[int]]
+    DRIVING_PULLEY_TEETH: List[int]
+    DRIVEN_PULLEY_TEETH: List[int]
+    HOME_POSITIONS: List[int]
+    JOINT_LIMITS: List[List[float]]
+    DH_PARAMS: dict
+    NAME: str
