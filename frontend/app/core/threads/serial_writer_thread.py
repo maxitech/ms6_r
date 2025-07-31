44b2b0e0ef76691cefbf4114d20c4e5b85a09bbc
maxitech <derstadlmax@gmail.com>
2025-07-31 10:18:43 +0200

    add: New thread for writing data to serial in 10ms intervals.

---
+++ frontend/app/core/threads/serial_writer_thread.py
@@ -0,0 +1,40 @@
+from PySide6.QtCore import QThread
+from queue import Queue, Empty
+import serial
+from oclock import Timer
+
+from app.core.shared.shared_data import shared_data
+
+
+class SerialWriterThread(QThread):
+    INTERVAL_S = 0.01  # 10ms
+
+    def __init__(self, serial: serial.Serial):
+        super().__init__()
+        self._serial_port = serial
+        self._is_running = True
+
+    def run(self):
+        print("SerialWriter started!")
+        tmr = Timer(interval=self.INTERVAL_S, precise=True)
+        while self._is_running:
+            try:
+                data = shared_data.get_next_data()
+            except Empty:
+                data = None
+
+            if data and self._serial_port:
+                try:
+                    self._serial_port.write(data)
+                except serial.SerialException as e:
+                    print(f"Serial error: {e}. Stopping writer thread.")
+                    self.stop()
+                except Exception as e:
+                    print(f"Unhandled error: {e}")
+                    self.stop()
+
+            tmr.checkpt()
+
+    def stop(self):
+        self._is_running = False
+        self.wait()
