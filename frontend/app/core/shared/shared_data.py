18e4359beba641ea3c195e1803a3eb6bfecc8dab
maxitech <derstadlmax@gmail.com>
2025-07-31 10:36:24 +0200

    add: Logic to provide signal based fuction calls (for UI updates, new calculations etc.).

--- frontend/app/core/shared/shared_data.py
+++ frontend/app/core/shared/shared_data.py
@@ -10,6 +10,20 @@ class SharedData:
         self._data_out: bytearray = None
         self._data_in: str = None
 
+        self._signals = {
+            "new_steps": [],
+        }
+
+    def emit_signal(self, signal_name, data):
+        for callback in self._signals[signal_name]:
+            callback(data)
+
+    def subscribe(self, signal_name, callback):
+        if signal_name in self._signals:
+            self._signals[signal_name].append(callback)
+        else:
+            print(f"No Signal: '{signal_name}' found.")
+
     def set_data_out(self, data: bytearray):
         with self._lock:
             self._data_out = data
