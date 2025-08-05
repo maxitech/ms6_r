d61df2ae8491ddce3d6d2c1e2eaacb7db8f10490
maxitech <derstadlmax@gmail.com>
2025-08-05 15:11:20 +0200

    add: New serial reader function -> reads data from serial and stores it in buffer. Changes due to com protocoll changes.(WIP)

--- backend/src/SerialHandler.cpp
+++ backend/src/SerialHandler.cpp
@@ -124,44 +124,77 @@ void SerialHandler::listenForSerial()
 
 void SerialHandler::_readSerialInput()
 {
-    static char   message[MAX_MESSAGE_SIZE];
-    static size_t index     = 0;
-    static bool   receiving = false;
+    static enum ParserState {
+        WAIT_FOR_START,
+        READ_LENGTH,
+        READ_PAYLOAD,
+        READ_CRC,
+        READ_END
+    } state = WAIT_FOR_START;
+
+    static uint8_t buffer[MAX_PACKAGE_SIZE];
+    static size_t  index         = 0;
+    static uint8_t payloadLength = 0;
 
     while (Serial.available() > 0)
     {
-        char c = Serial.read();
+        uint8_t byte = Serial.read();
 
-        if (c == '$') // Start of message
+        switch (state)
         {
-            receiving = true;
-            index     = 0; // Reset buffer
-        }
-
-        if (receiving)
-        {
-            if (index < MAX_MESSAGE_SIZE - 1)
+        case WAIT_FOR_START:
+            buffer[index++] = byte;
+            if (index >= 3 &&
+                buffer[index - 3] == START_BYTES[0] &&
+                buffer[index - 2] == START_BYTES[1] &&
+                buffer[index - 1] == START_BYTES[2])
             {
-                message[index++] = c;
-                if (c == '#') // End of message
-                {
-                    message[index] = '\0';
-                    receiving      = false;
-
-                    String input = String(message);
-                    input.trim();
-
-                    _forwardInput(input); // Process input
-                    index = 0;
-                }
+                index = 3; // Start of actual payload (next byte will be LEN)
+                state = READ_LENGTH;
             }
-            else
+            else if (index > 3)
+            // Sliding window: keep last 2 bytes, write new byte at buffer[2]
             {
-                // Buffer overflow
-                receiving = false;
-                index     = 0;
-                Serial.println("Error: Serial message too long");
+                buffer[0] = buffer[index - 2];
+                buffer[1] = buffer[index - 1];
+                buffer[2] = byte;
+                index     = 3;
             }
+            break;
+        case READ_LENGTH:
+            payloadLength   = byte;
+            buffer[index++] = byte;
+            // if buffer size not packet len -> timeout
+            state = READ_PAYLOAD;
+            break;
+        case READ_PAYLOAD:
+            buffer[index++] = byte;
+            if (index >= 4 + payloadLength)
+            {
+                state = READ_CRC;
+            }
+            break;
+        case READ_CRC:
+            buffer[index++] = byte;
+            if (index >= 4 + payloadLength + 2)
+            {
+                state = READ_END;
+            }
+            break;
+
+        case READ_END:
+            buffer[index++] = byte;
+            if (index >= 4 + payloadLength + 4)
+            {
+                // validate crc and end bytes
+                // process payload
+                state = WAIT_FOR_START;
+                index = 0;
+            }
+            break;
+        default:
+            Serial.println("Unexpected error in 'SerialHandler.cpp'");
+            break;
         }
     }
 }
