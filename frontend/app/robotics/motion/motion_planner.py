aa2be90e191b438bac7b2ab4b85eb515db8d7eb1
maxitech <derstadlmax@gmail.com>
2025-08-07 16:53:24 +0200

    add: Motion planner class.

---
+++ frontend/app/robotics/motion/motion_planner.py
@@ -0,0 +1,15 @@
+class MotionPlanner:
+    def __init__(self):
+        pass
+
+    # *** Public ***
+    def jogJoint(self, dir: int, speed: int):
+        # calc dir_vel = dir * speed
+        # get steps -> check for limits
+        # if limit exceeded -> stop else ok
+        pass
+
+    def log(self, data):
+        print("Log: ", data)
+
+    # *** Private ***
