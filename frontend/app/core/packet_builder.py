64eb085dd09e578a702f6b00375a4e5edd5516d1
maxitech <derstadlmax@gmail.com>
2025-08-11 13:49:40 +0200

    add: Packet builder function which is responsible for creating a packet with the correct structure.

--- frontend/app/core/packet_builder.py
+++ frontend/app/core/packet_builder.py
@@ -1,3 +1,4 @@
+import crcmod
 from app.constants.com_protocoll import *
 from typing import List, Callable, Any
 
@@ -11,24 +12,30 @@ class PacketBuilder:
             CMD_JOG: self._pack_jog_speeds,
         }
 
-    def build_packet(self, cmd_id: int, data) -> bytes:
-        pack_fn: Callable[[Any], bytes] | None = self.payload_map.get(cmd_id)
-        if not pack_fn:
-            raise ValueError(f"Unknown command ID: '{cmd_id}'")
-        dynamic_payload: bytes = pack_fn(data)
-        fix_payload: bytes = bytes([NOP])
-        payload: bytes = bytes([cmd_id]) + dynamic_payload + fix_payload
-        p_len: int = len(payload)
-        crc: int = 0
+        def build_packet(self, cmd_id: int, data) -> bytes:
+            if not 0 <= cmd_id <= 255:
+                raise ValueError("cmd_id must be 0-255")
+            pack_fn: Callable[[Any], bytes] | None = self.payload_map.get(cmd_id)
+            if not pack_fn:
+                raise ValueError(f"Unknown command ID: '{cmd_id}'")
+            dynamic_payload: bytes = pack_fn(data)
+            fix_payload: bytes = NOP.to_bytes(1, "big", signed=False)
+            fpl_len = len(fix_payload).to_bytes(1, "big", signed=False)
+            payload: bytes = (
+                cmd_id.to_bytes(1, "big", signed=False)
+                + dynamic_payload
+                + fix_payload
+                + fpl_len
+            )
+            pl_len: bytes = len(payload).to_bytes(1, "big", signed=False)
+            crc16_fn = crcmod.predefined.mkCrcFun("modbus")
+            crc_value: int = crc16_fn(payload)
+            crc_bytes: bytes = crc_value.to_bytes(2, byteorder="little")
 
-        packet = (
-            bytes(START_BYTES)
-            + bytes([p_len])
-            + payload
-            + bytes([crc])
-            + bytes(END_BYTES)
-        )
-        return packet
+            packet = (
+                bytes(START_BYTES) + pl_len + payload + crc_bytes + bytes(END_BYTES)
+            )
+            return packet
 
     def _int_to_3_bytes(self, val: int) -> bytes:
         if not self.SIGNED_24BIT_MIN <= val <= self.SIGNED_24BIT_MAX:
@@ -38,6 +45,7 @@ class PacketBuilder:
         return val.to_bytes(length=3, byteorder="big", signed=True)
 
     def _pack_jog_speeds(self, speeds: List[int]) -> bytes:
+        print(speeds)
         payload = bytearray()
         for val in speeds:
             payload.extend(self._int_to_3_bytes(val))
