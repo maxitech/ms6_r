bdcdb97a007fba3f01ca493fb066a12035a0ebe6
maxitech <derstadlmax@gmail.com>
2025-08-26 09:58:03 +0200

    add: Improved error output message with detailed information for the user.

--- frontend/app/core/packet_processor.py
+++ frontend/app/core/packet_processor.py
@@ -4,6 +4,7 @@ from app.constants.com_protocol import *
 
 class PacketProcessor:
     def __init__(self):
+        self._cmd_id = None
         self._status = None
         self._detail = None
 
@@ -16,7 +17,7 @@ class PacketProcessor:
         pl_len = raw_data[3]
         payload = raw_data[4 : 4 + pl_len]
 
-        cmd_id = payload[0]
+        self._cmd_id = payload[0]
         self._status = payload[1]
         self._detail = payload[2]
 
@@ -28,17 +29,11 @@ class PacketProcessor:
                     case _ if self._detail == DATA_STEPS:
                         pass  # Aktion DATA_STEPS
                     case _ if self._detail == INFO_LOADED_PROGRAM:
-                        print(
-                            f"[INFO] Program: {payload[3].to_bytes(1, "big").hex()} loaded."
-                        )
+                        print(f"[INFO] Program: 0x{payload[3]:02X} loaded.")
                     case _ if self._detail == INFO_RELOADED_PROGRAM:
-                        print(
-                            f"[INFO] Program: {payload[3].to_bytes(1, "big").hex()} reloaded."
-                        )
+                        print(f"[INFO] Program: 0x{payload[3]:02X} reloaded.")
                     case _ if self._detail == INFO_ALREADY_RUNNING:
-                        print(
-                            f"[INFO] Program: {payload[3].to_bytes(1, "big").hex()} already running."
-                        )
+                        print(f"[INFO] Program: 0x{payload[3]:02X} already running.")
                     case _ if self._detail == INFO_HOMING_ALREADY_DONE:
                         print("[INFO] Homing already done.")
                     case _ if self._detail == WARN_NOP_IGNORED:
@@ -55,34 +50,48 @@ class PacketProcessor:
                 match self._detail:
                     case _ if self._detail == ERR_UNKNOWN:
                         print("[ERROR] Unknown error occurred.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_INVALID_PACKET:
                         print("[ERROR] Invalid packet received.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_TIMEOUT:
                         print("[ERROR] Operation timed out.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_CHECKSUM:
                         print("[ERROR] Checksum error.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_NO_PROCESSOR:
                         print("[ERROR] No processor available.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_BUFFER_OVERFLOW:
                         print("[ERROR] Buffer overflow.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_INVALID_CMD:
                         print("[ERROR] Invalid command.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_INVALID_TELEMETRY_FLAG:
                         print("[ERROR] Invalid telemetry flag.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_UNKNOWN_PROGRAM:
                         print(
                             f"[ERROR] Program: {payload[3].to_bytes(1, "big").hex()} unknown."
                         )
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_INDEX_OOB:
                         print("[ERROR] Index out of bounds.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_INVALID_DIR:
                         print("[ERROR] Invalid direction.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_JSON_PARSE:
                         print("[ERROR] JSON parse error.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_ALLOC_FAIL:
                         print("[ERROR] Allocation failed.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _ if self._detail == ERR_MISSING_FIELD:
                         print("[ERROR] Missing field.")
+                        self._logErrorDetail(self._cmd_id, self._status, self._detail)
                     case _:
                         print("[ERROR] Unknown STATUS_ERROR detail.")
             case _:
@@ -118,3 +127,9 @@ class PacketProcessor:
             return False
 
         return True
+
+    def _logErrorDetail(self, cmd_id: int, status: int, detail: int):
+        print(f"Command ID: 0x{cmd_id:02X}")
+        print(f"Status:     0x{status:02X}")
+        print(f"Detail:     0x{detail:02X}")
+        print("-----------------------------------------------------------------")
